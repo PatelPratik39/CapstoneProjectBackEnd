c42123fcccfd7af28fe4be465df6c9009eca148a
Jessica Maddux <jessicamaddux@Jessicas-Air.lan>
2024-04-23 21:12:42 -0500

    got get movies by movieid working!

--- api/utils.js
+++ api/utils.js
@@ -1,53 +1,53 @@
 ////THIS IS DEMO CODE! NEED TO REPLACE WITH OUR CODE!!
-// const client = require('../db/client');
+const client = require('../db/client');
 
-// function requireUser(req, res, next) {
-//   if (!req.user) {
-//     res.status(401);
-//     next({
-//       name: "MissingUserError",
-//       message: "You must be logged in to perform this action"
-//     });
-//   }
+function requireUser(req, res, next) {
+  if (!req.user) {
+    res.status(401);
+    next({
+      name: "MissingUserError",
+      message: "You must be logged in to perform this action"
+    });
+  }
 
-//   next();
-// }
+  next();
+}
 
-// // takes required parameters as an array, returns a middleware function that sends back a message if they're not present
-// const requiredNotSent = ({ requiredParams, atLeastOne = false }) => {
-//   return (req, res, next) => {
-//     // for operations that need at least one param. Not all required.
-//     if (atLeastOne) {
-//       let numParamsFound = 0;
-//       for (let param of requiredParams) {
-//         if (req.body[param] !== undefined) {
-//           numParamsFound++;
-//         }
-//       }
-//       if (!numParamsFound) {
-//         next({
-//           name: 'MissingParams',
-//           message: `Must provide at least one of these in body: ${requiredParams.join(', ')}`
-//         })
-//       } else {
-//         next();
-//       }
-//     } else {
-//       // figure out which ones are not defined, and return them
-//       const notSent = [];
-//       for (let param of requiredParams) {
-//         if (req.body[param] === undefined) {
-//           notSent.push(param);
-//         }
-//       }
-//       if (notSent.length) next({
-//         name: 'MissingParams',
-//         message: `Required Parameters not sent in body: ${notSent.join(', ')}`
-//       })
-//       next();
-//     }
-//   }
-// }
+// takes required parameters as an array, returns a middleware function that sends back a message if they're not present
+const requiredNotSent = ({ requiredParams, atLeastOne = false }) => {
+  return (req, res, next) => {
+    // for operations that need at least one param. Not all required.
+    if (atLeastOne) {
+      let numParamsFound = 0;
+      for (let param of requiredParams) {
+        if (req.body[param] !== undefined) {
+          numParamsFound++;
+        }
+      }
+      if (!numParamsFound) {
+        next({
+          name: 'MissingParams',
+          message: `Must provide at least one of these in body: ${requiredParams.join(', ')}`
+        })
+      } else {
+        next();
+      }
+    } else {
+      // figure out which ones are not defined, and return them
+      const notSent = [];
+      for (let param of requiredParams) {
+        if (req.body[param] === undefined) {
+          notSent.push(param);
+        }
+      }
+      if (notSent.length) next({
+        name: 'MissingParams',
+        message: `Required Parameters not sent in body: ${notSent.join(', ')}`
+      })
+      next();
+    }
+  }
+}
 // //CHECK THIS FUNCTION--I DONT THINK WE NEED THIS....
 // // calculate total price of rental by taking the bike id, rental dates, and price per day
 // // async function calculateRentalPrice(bike_id, rental_date_from, rental_date_to) {
@@ -88,9 +88,9 @@
 // //   }
 // // }
 
-// module.exports = {
-//   requireUser,
-//   requiredNotSent,
-//   // calculateRentalPrice,
-//   // checkRentalExists
-// }
+module.exports = {
+  requireUser,
+  requiredNotSent,
+  // calculateRentalPrice,
+  // checkRentalExists
+}
