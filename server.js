f58f76ce09333939b134cf3e0555b71748a07af8
Sequoia Nevae <sequoianevae@Sequoias-iMac.lan>
2024-05-02 21:04:58 -0500

    add allow methods

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ server.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
 
   // Request methods you wish to allow
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
